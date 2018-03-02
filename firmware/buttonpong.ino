51a96626cc0fd68029c8fae271139ae64c099ad7
David Washington <david.washington@microsoft.com>
2018-03-02 09:53:17 -0600

    add gamestate

--- firmware/buttonpong.ino
+++ firmware/buttonpong.ino
@@ -4,7 +4,7 @@
 
 #define INTERNET_BUTTON_TYPE 0
 
-bool isRegistered = false;
+int gameState = 0; // 0 - gameover, 1 - waiting
 
 InternetButton b = InternetButton();
 InternetButtonEvents buttonEvents = InternetButtonEvents(b);
@@ -22,9 +22,8 @@ InternetButtonEvents buttonEvents = InternetButtonEvents(b);
 void setup() {
     Serial.begin(9600);
     Particle.function("ping", ping);
-    Particle.subscribe("hook-response/pong", registrationHandler);
     
-    buttonEvents.onButtonOn(buttonOnHandler);
+    Particle.subscribe("hook-response/pong", registrationHandler);
     buttonEvents.onButtonClicked(buttonClickedHandler);
     buttonEvents.onAllButtonsClicked(allButtonsClickedHandler);
 
@@ -35,38 +34,75 @@ void setup() {
 void loop(){
     buttonEvents.update();
     
-    if (buttonEvents.allButtonsOn()) {
+    if (gameState == 0 && buttonEvents.allButtonsOn()) {
         b.allLedsOn(0,20,20);
-    } else {
+    } else if (gameState == 1) {
+        if (buttonEvents.buttonOn(1)) {
+            b.ledOn(1,0,20,0);
+            b.ledOn(11,0,20,0);
+        } else {
+            b.ledOff(1);
+            b.ledOff(11);
+        }
+        
+        if (buttonEvents.buttonOn(2)) {
+            b.ledOn(2,0,0,20);
+            b.ledOn(3,0,0,20);
+            b.ledOn(4,0,0,20);
+        } else {
+            b.ledOff(2);
+            b.ledOff(3);
+            b.ledOff(4);
+        }
+        
+        if (buttonEvents.buttonOn(3)) {
+            b.ledOn(5,20,0,0);
+            b.ledOn(6,20,0,0);
+            b.ledOn(7,20,0,0);
+        } else {
+            b.ledOff(5);
+            b.ledOff(6);
+            b.ledOff(7);
+        }
+        
+        if (buttonEvents.buttonOn(4)) {
+            b.ledOn(8,20,20,0);
+            b.ledOn(9,20,20,0);
+            b.ledOn(10,20,20,0);
+        } else {
+            b.ledOff(8);
+            b.ledOff(9);
+            b.ledOff(10);
+        }
+
+    }
+    else {
         b.allLedsOff();
     }
 }
 
 void buttonOnHandler(int buttonNumber) {
-    Serial.print("down: ");
-    Serial.println(buttonNumber);
+
 }
 
 void allButtonsClickedHandler() {
-    b.allLedsOn(20,0,0);
     
-    Particle.publish("register", "TRUE");
-    Serial.println("all click");
-    delay(500);
+    if (gameState == 0) {
+        Particle.publish("register");
+    }
+
 }
 
 void buttonClickedHandler(int buttonNumber) {
-    b.allLedsOn(20,20,0);
-    
-    //TBD
-    //Particle.publish("ping", "TRUE");
-    Serial.print("click: ");
-    Serial.println(buttonNumber);
-    delay(500);
+    if (gameState == 1) {
+        Particle.publish("ping", "TRUE");
+    }
 }
 
 void registrationHandler(const char *event, const char *data) {
-    isRegistered = true;
+    gameState = 1;
+    b.allLedsOn(0,20,0);
+    delay(500);
 }
 
 int ping(String countdown) {
