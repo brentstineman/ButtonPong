9465b3453cc150775557dd423f32e3a7a1582673
David Washington <david.washington@microsoft.com>
2018-03-02 19:55:59 -0600

    Add the READY game state; remove registration callback

--- firmware/buttonpong.ino
+++ firmware/buttonpong.ino
@@ -3,8 +3,9 @@
 #include <InternetButton.h>
 
 #define STATE_GAME_OVER         0
-#define STATE_GAME_WAITING      1
-#define STATE_GAME_RESPONDING   2
+#define STATE_GAME_READY        1
+#define STATE_GAME_WAITING      2
+#define STATE_GAME_RESPONDING   3
 
 int gameState = STATE_GAME_OVER;
 unsigned long respondingTimeout;
@@ -15,8 +16,8 @@ InternetButtonEvents buttonEvents = InternetButtonEvents(b);
 void setup() {
     Serial.begin(9600);
     
+    Particle.function("startGame", startGame);
     Particle.function("ping", ping);
-    Particle.subscribe("hook-sent/register", registrationHandler, MY_DEVICES);
     
     buttonEvents.onButtonClicked(buttonClickedHandler);
     buttonEvents.onAllButtonsClicked(allButtonsClickedHandler);
@@ -31,12 +32,15 @@ void loop(){
 }
 
 void updateGameLeds() {
-      if (gameState == STATE_GAME_OVER && buttonEvents.allButtonsOn()) {
+    if (gameState == STATE_GAME_OVER && buttonEvents.allButtonsOn()) {
         b.allLedsOn(0,20,20);
-    } else if (gameState == STATE_GAME_RESPONDING) {
+        
+    } else if (gameState == STATE_GAME_READY || 
+               gameState == STATE_GAME_RESPONDING) {
+        
         if (buttonEvents.buttonOn(1)) {
-            b.ledOn(1,0,20,0);
-            b.ledOn(11,0,20,0);
+            b.ledOn(1,0,0,20);
+            b.ledOn(11,0,0,20);
         } else {
             b.ledOff(1);
             b.ledOff(11);
@@ -53,9 +57,9 @@ void updateGameLeds() {
         }
         
         if (buttonEvents.buttonOn(3)) {
-            b.ledOn(5,20,0,0);
-            b.ledOn(6,20,0,0);
-            b.ledOn(7,20,0,0);
+            b.ledOn(5,0,0,20);
+            b.ledOn(6,0,0,20);
+            b.ledOn(7,0,0,20);
         } else {
             b.ledOff(5);
             b.ledOff(6);
@@ -63,9 +67,9 @@ void updateGameLeds() {
         }
         
         if (buttonEvents.buttonOn(4)) {
-            b.ledOn(8,20,20,0);
-            b.ledOn(9,20,20,0);
-            b.ledOn(10,20,20,0);
+            b.ledOn(8,0,0,20);
+            b.ledOn(9,0,0,20);
+            b.ledOn(10,0,0,20);
         } else {
             b.ledOff(8);
             b.ledOff(9);
@@ -83,22 +87,34 @@ void allButtonsClickedHandler() {
     if (gameState == STATE_GAME_OVER) {
         Serial.println("Registering for new game");
         Particle.publish("register");
+        gameState = STATE_GAME_READY;
     }
 
 }
 
-void registrationHandler(const char *event, const char *data) {
-    Serial.println("Registration succeeded");
-
-    gameState = STATE_GAME_WAITING;
-    b.allLedsOn(0,20,0);
-    delay(500);
+int startGame(String args) {
+    Serial.println("New game started");
+    
+    int val = -1;
+    
+    if (gameState == STATE_GAME_READY) {
+        gameState = STATE_GAME_WAITING;
+        val = 1;
+        
+        b.allLedsOn(0,20,0);
+        delay(2000);
+    }
+    
+    return val;
 }
 
 int ping(String timeout) {
     Serial.println("Received move: " + timeout + " milliseconds");
-    
+    int val = -1;
+
     if (gameState == STATE_GAME_WAITING) {
+        val = 1;
+        
         int timeoutVal = 5000;
         if (timeout != NULL) {
             timeoutVal = timeout.toInt();
@@ -114,32 +130,37 @@ int ping(String timeout) {
             respondingTimeout = millis() + timeoutVal;
             gameState = STATE_GAME_RESPONDING;
         }
-        
       
     }
     
-    return 1;
+    return val;
 }
 
 void buttonClickedHandler(int buttonNumber) {
-    if (gameState == STATE_GAME_WAITING) {
+    Serial.println(gameState);
+    
+    if (gameState == STATE_GAME_READY) {
+        Serial.println("Kicking off a game");
+
+        Particle.publish("Go");
     } else if (gameState == STATE_GAME_RESPONDING) {
-        Serial.println("Playing a move");
 
         if (millis() < respondingTimeout) {
             Serial.println("Successful move");
             Particle.publish("pong", "TRUE");
             gameState = STATE_GAME_WAITING;
             b.allLedsOn(0,20,0);
+            delay(500);
+
         } else {
             // Game over
             Serial.println("Game over");
             Particle.publish("pong", "FALSE");           
             gameState = STATE_GAME_OVER;
             b.allLedsOn(20,0,0);
+            delay(2000);
         }
         
         respondingTimeout = 0;
-        delay(500);
     }
 }
