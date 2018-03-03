7872481ec332afc99aa7145dd372b141235d3fb5
David Washington <david.washington@microsoft.com>
2018-03-02 23:01:40 -0600

    Fancy countdown. auto gameover on timeout

--- firmware/buttonpong.ino
+++ firmware/buttonpong.ino
@@ -35,50 +35,74 @@ void updateGameLeds() {
     if (gameState == STATE_GAME_OVER && buttonEvents.allButtonsOn()) {
         b.allLedsOn(0,20,20);
         
-    } else if (gameState == STATE_GAME_READY || 
-               gameState == STATE_GAME_RESPONDING) {
-        
-        if (buttonEvents.buttonOn(1)) {
-            b.ledOn(1,0,0,20);
-            b.ledOn(11,0,0,20);
-        } else {
-            b.ledOff(1);
-            b.ledOff(11);
-        }
-        
-        if (buttonEvents.buttonOn(2)) {
-            b.ledOn(2,0,0,20);
-            b.ledOn(3,0,0,20);
-            b.ledOn(4,0,0,20);
-        } else {
-            b.ledOff(2);
-            b.ledOff(3);
-            b.ledOff(4);
-        }
-        
-        if (buttonEvents.buttonOn(3)) {
-            b.ledOn(5,0,0,20);
-            b.ledOn(6,0,0,20);
-            b.ledOn(7,0,0,20);
-        } else {
-            b.ledOff(5);
-            b.ledOff(6);
-            b.ledOff(7);
-        }
-        
-        if (buttonEvents.buttonOn(4)) {
-            b.ledOn(8,0,0,20);
-            b.ledOn(9,0,0,20);
-            b.ledOn(10,0,0,20);
-        } else {
-            b.ledOff(8);
-            b.ledOff(9);
-            b.ledOff(10);
-        }
-
+    } else if (gameState == STATE_GAME_READY) {
+        updateButtonPressLeds();
+    } else if (gameState == STATE_GAME_RESPONDING) {
+        updateButtonPressLeds();
+        updateCoundown();
     }
     else {
         b.allLedsOff();
+    }
+}
+
+void updateCoundown() {
+    
+    if (millis() > respondingTimeout) {
+        // Game Over
+        Particle.publish("pong", "FALSE");           
+
+        b.allLedsOn(20,0,0);
+        delay(2000);
+        gameState = STATE_GAME_OVER;
+        
+    } else {
+        int secondsRemain = ((respondingTimeout - millis()) / 1000);
+
+        for (int i = 0; i < secondsRemain && i < 11; i++) {
+            b.ledOn(i+1, 0, 20, 0);
+        }
+        
+    }
+}
+
+void updateButtonPressLeds() {
+    if (buttonEvents.buttonOn(1)) {
+            b.ledOn(1,0,0,20);
+            b.ledOn(11,0,0,20);
+    } else {
+        b.ledOff(1);
+        b.ledOff(11);
+    }
+    
+    if (buttonEvents.buttonOn(2)) {
+        b.ledOn(2,0,0,20);
+        b.ledOn(3,0,0,20);
+        b.ledOn(4,0,0,20);
+    } else {
+        b.ledOff(2);
+        b.ledOff(3);
+        b.ledOff(4);
+    }
+    
+    if (buttonEvents.buttonOn(3)) {
+        b.ledOn(5,0,0,20);
+        b.ledOn(6,0,0,20);
+        b.ledOn(7,0,0,20);
+    } else {
+        b.ledOff(5);
+        b.ledOff(6);
+        b.ledOff(7);
+    }
+    
+    if (buttonEvents.buttonOn(4)) {
+        b.ledOn(8,0,0,20);
+        b.ledOn(9,0,0,20);
+        b.ledOn(10,0,0,20);
+    } else {
+        b.ledOff(8);
+        b.ledOff(9);
+        b.ledOff(10);
     }
 }
 
@@ -137,30 +161,19 @@ int ping(String timeout) {
 }
 
 void buttonClickedHandler(int buttonNumber) {
-    Serial.println(gameState);
-    
+
     if (gameState == STATE_GAME_READY) {
         Serial.println("Kicking off a game");
 
         Particle.publish("go");
     } else if (gameState == STATE_GAME_RESPONDING) {
 
-        if (millis() < respondingTimeout) {
-            Serial.println("Successful move");
-            Particle.publish("pong", "TRUE");
-            gameState = STATE_GAME_WAITING;
-            b.allLedsOn(0,20,0);
-            delay(500);
+        Serial.println("Successful move");
+        Particle.publish("pong", "TRUE");
+        gameState = STATE_GAME_WAITING;
+        b.allLedsOn(0,20,0);
+        delay(500);
 
-        } else {
-            // Game over
-            Serial.println("Game over");
-            Particle.publish("pong", "FALSE");           
-            gameState = STATE_GAME_OVER;
-            b.allLedsOn(20,0,0);
-            delay(2000);
-        }
-        
         respondingTimeout = 0;
     }
 }
